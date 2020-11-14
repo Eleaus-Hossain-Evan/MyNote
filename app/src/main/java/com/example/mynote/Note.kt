8d32e276fad354d450adee2e6192603d766f62f6
Eleaus-Hossain-Evan <70533691+Eleaus-Hossain-Evan@users.noreply.github.com>
2020-11-15 03:44:15 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mynote/Note.kt
@@ -0,0 +1,11 @@
+package com.example.mynote
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes_table")
+class Note(@ColumnInfo(name = "text") val test: String) {
+
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+}
---
+++ app/src/main/java/com/example/mynote/NoteDao.kt
@@ -0,0 +1,19 @@
+package com.example.mynote
+
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("DELETE FROM notes_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM notes_table ORDER BY text ASC")
+    fun getAlphabetizedWords(): List<Note>
+}
